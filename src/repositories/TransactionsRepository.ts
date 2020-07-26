712fe52f2863fd1a889e3ae83561e17c8229e544
andreflp <flpandresilva@gmail.com>
2020-07-26 00:36:06 -0300

    ADD: implementing routes and services

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,17 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransactionDTO { 
+  title: string
+  value: number
+  type: 'income' | 'outcome'
+}
+
+interface Response {
+  transactions: Transaction[]
+  balance: Balance
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +25,37 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions
   }
 
   public getBalance(): Balance {
-    // TODO
+    const income = this.transactions.reduce((total, transaction) => {
+      if(transaction.type === 'income') return total += transaction.value
+      else return total
+    }, 0)
+
+    const outcome = this.transactions.reduce((total, transaction) => {
+      if(transaction.type === 'outcome') return total += transaction.value
+      else return total
+    }, 0)
+
+    const total = income - outcome
+    
+    const balance = {
+      income,
+      outcome,
+      total
+    }
+
+    return balance
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransactionDTO): Transaction {
+    const transaction = new Transaction({ title, value, type })
+
+    this.transactions.push(transaction)
+    
+    return transaction
   }
 }
 
